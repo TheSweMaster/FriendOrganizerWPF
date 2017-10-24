d403b715415a9f94667714a34465723328cbd925
Jacob Gandemo <theswemaster@gmail.com>
2017-10-24 23:09:58 +0200

    Added UI-Layer with MVVM-pattern, Test DataService, Autofac Dep Injc

---
+++ FriendOrganizer/FriendOrganizer.UI/Data/FriendDataService.cs
@@ -0,0 +1,18 @@
+﻿using FriendOrganizer.Model;
+using System.Collections.Generic;
+
+namespace FriendOrganizer.UI.Data
+{
+    public class FriendDataService : IFriendDataService
+    {
+        public IEnumerable<Friend> GetAll()
+        {
+            // TODO: Load data from database
+            yield return new Friend { FirstName = "Jacob", LastName = "Gandis" };
+            yield return new Friend { FirstName = "Anders", LastName = "Svensson" };
+            yield return new Friend { FirstName = "Samuel", LastName = "Samuelsson" };
+            yield return new Friend { FirstName = "Julia", LastName = "Assange" };
+        }
+
+    }
+}
