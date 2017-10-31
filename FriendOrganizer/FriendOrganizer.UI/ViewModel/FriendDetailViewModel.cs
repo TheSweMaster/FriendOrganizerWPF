5fa91bb94fc99fa9ed932fa00fb4142ea1539807
Jacob Gandemo <theswemaster@gmail.com>
2017-10-31 12:47:51 +0100

    Communicating Between ViewModels, Prism's EventAggregator

--- FriendOrganizer/FriendOrganizer.UI/ViewModel/FriendDetailViewModel.cs
+++ FriendOrganizer/FriendOrganizer.UI/ViewModel/FriendDetailViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using FriendOrganizer.Model;
 using FriendOrganizer.UI.Data;
+using FriendOrganizer.UI.Event;
+using Prism.Events;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,10 +13,20 @@ namespace FriendOrganizer.UI.ViewModel
     public class FriendDetailViewModel : ViewModelBase, IFriendDetailViewModel
     {
         private IFriendDataService _dataService;
+        private IEventAggregator _eventAggregator;
 
-        public FriendDetailViewModel(IFriendDataService dataService)
+        public FriendDetailViewModel(IFriendDataService dataService,
+            IEventAggregator eventAggregator)
         {
             _dataService = dataService;
+            _eventAggregator = eventAggregator;
+            _eventAggregator.GetEvent<OpenFriendDetailViewEvent>()
+                .Subscribe(OnOpenFriendDetailViewAsync);
+        }
+
+        private async void OnOpenFriendDetailViewAsync(int friendId)
+        {
+            await LoadAsync(friendId);
         }
 
         private Friend _friend;
--- FriendOrganizer/FriendOrganizer.UI/ViewModel/NavigationViewModel.cs
+++ FriendOrganizer/FriendOrganizer.UI/ViewModel/NavigationViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using FriendOrganizer.Model;
 using FriendOrganizer.UI.Data;
+using FriendOrganizer.UI.Event;
+using Prism.Events;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -9,15 +11,18 @@ using System.Threading.Tasks;
 
 namespace FriendOrganizer.UI.ViewModel
 {
-    public class NavigationViewModel : INavigationViewModel
+    public class NavigationViewModel : ViewModelBase, INavigationViewModel
     {
         private IFriendLookUpDataService _friendLookUpDataService;
+        private IEventAggregator _eventAggregator;
 
         public ObservableCollection<LookUpItem> Friends { get; }
 
-        public NavigationViewModel(IFriendLookUpDataService friendLookUpDataService)
+        public NavigationViewModel(IFriendLookUpDataService friendLookUpDataService, 
+            IEventAggregator eventAggreator)
         {
             _friendLookUpDataService = friendLookUpDataService;
+            _eventAggregator = eventAggreator;
             Friends = new ObservableCollection<LookUpItem>();
         }
 
@@ -28,6 +33,23 @@ namespace FriendOrganizer.UI.ViewModel
             foreach (var item in lookup)
             {
                 Friends.Add(item);
+            }
+        }
+
+        private LookUpItem _selectedFriend;
+
+        public LookUpItem SelectedFriend
+        {
+            get { return _selectedFriend; }
+            set
+            {
+                _selectedFriend = value;
+                OnPropertyChanged();
+                if (_selectedFriend != null)
+                {
+                    _eventAggregator.GetEvent<OpenFriendDetailViewEvent>()
+                        .Publish(_selectedFriend.Id);
+                }
             }
         }
 
